75ee241eaaf1af615242af5a69add0ad0e69b78b
ani67h <ghoshanindya4589@gmail.com>
2026-07-03 20:55:34 +0600

    interface

---
+++ myinterface.java
@@ -0,0 +1,23 @@
+interface MyInterface{
+    /*compiler will treat them as:
+    public abstract void method1();
+    public abstract void method2(); */
+    public void method1();
+    public void method2();
+}
+class Main implements MyInterface{
+    /*this class must have to implement both the abstract methods
+    else you will get compilation error
+    try removing one method and see the error*/
+    public void method1(){
+        System.out.println("implement of method1");
+    }
+    public void method2(){
+        System.out.println("implement of mehtod2");
+    }
+    public static void main (String args[]){
+        MyInterface obj = new Main();
+        obj.method1();
+        obj.method2();
+    }
+}
